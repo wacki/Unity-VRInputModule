82b7a248a19d3f93ee0d707834c78d1d4eb7f13c
wacki <wackigit@gmail.com>
2016-05-31 11:11:18 +0200

    init

---
+++ Assets/ViveLaserPointerGUI/Scripts/ViveUILaserPointer.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using Valve.VR;
+
+namespace Wacki {
+
+    public class ViveUILaserPointer : IUILaserPointer {
+
+        public EVRButtonId button = EVRButtonId.k_EButton_SteamVR_Trigger;
+
+        private SteamVR_TrackedObject _trackedObject;
+        private bool _connected = false;
+        private int _index;
+
+        protected override void Initialize()
+        {
+            base.Initialize();
+            Debug.Log("Initialize");
+
+            var trackedObject = GetComponent<SteamVR_TrackedObject>();
+
+            if(trackedObject != null) {
+                _index = (int)trackedObject.index;
+                _connected = true;
+            }
+        }
+
+        public override bool ButtonDown()
+        {
+            if(!_connected)
+                return false;
+
+            var device = SteamVR_Controller.Input(_index);
+            if(device != null) {
+                var result = device.GetPressDown(button);
+                return result;
+            }
+
+            return false;
+        }
+
+        public override bool ButtonUp()
+        {
+            if(!_connected)
+                return false;
+
+            var device = SteamVR_Controller.Input(_index);
+            if(device != null)
+                return device.GetPressUp(button);
+
+            return false;
+        }
+
+        protected override void OnEnterControl(GameObject control)
+        {
+            // todo...
+        }
+
+        protected override void OnExitControl(GameObject control)
+        {
+            // todo...
+        }
+    }
+
+}
